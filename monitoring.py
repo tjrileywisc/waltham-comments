8742175ec04fe408d809c8e15a0df67abe21aa87
Timothy Riley <timothyjohnriley@gmail.com>
2026-06-12 10:43:53 -0400

    Normalize logging

--- monitoring.py
+++ monitoring.py
@@ -1,19 +1,19 @@
 
+import json
 import logging
+import logging.config
+
 
 def setup_logging(logger_name: str | None) -> logging.Logger:
+    with open("log_config.json") as f:
+        config = json.load(f)
 
-    formatter = logging.Formatter(
-        fmt="%(asctime)s %(name)s %(levelname)-8s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
-    )
+    if logger_name:
+        config["loggers"][logger_name] = {
+            "handlers": ["default"],
+            "level": "DEBUG",
+            "propagate": False,
+        }
 
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    console_handler.setLevel(logging.DEBUG)
-
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(console_handler)
-
-    return logger
+    logging.config.dictConfig(config)
+    return logging.getLogger(logger_name)
